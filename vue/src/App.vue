2caf9156d5e4d864f5654e5b17f61a9a5285a004
Tyler Rash <rash_tyler@yahoo.com>
2020-12-09 16:09:42 -0500

    vue changes, initial structure for homepage implimented

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,13 +1,53 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link
-        v-bind:to="{ name: 'logout' }"
-        v-if="$store.state.token != ''"
-        >Logout</router-link
-      >
+    <div class="shadow">
+      <ul class="nav nav-tabs">
+        <li class="nav-item">   
+          <router-link class="nav-link active" 
+          v-bind:to="{ name: 'home' }">Home</router-link>
+        </li>
+        <li class="nav-item" >
+          <router-link class="nav-link" 
+          v-bind:to="{ name: 'home' }" v-show="$store.state.token != ''">Account</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" 
+          v-bind:to="{ name: 'home' }"  v-show="$store.state.token != ''">Freinds</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" 
+          v-bind:to="{ name: 'home' }"  v-show="$store.state.token != ''">Trades</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" 
+          v-bind:to="{ name: 'home' }" v-show="$store.state.token != ''">Leaderboards</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" 
+          v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+        </li>
+      </ul>
     </div>
-    <router-view />
+    <router-view/>
   </div>
 </template>
+
+<style scoped>
+  #app{
+    background-color: cornflowerblue; /* change color to image later */
+  }
+  .nav-item {
+    background-color:darkgrey;
+  }
+  .nav-link {
+    color: white;
+  }
+  .active.nav-link{
+    background-color: lightgrey;
+  }
+  .nav-tabs{
+    width: 75%;
+    margin: auto;
+    border-bottom: none;
+  }
+</style>
