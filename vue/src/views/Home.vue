87bab70c39502ac20914fc409529f43351e81aa2
Tyler Rash <rash_tyler@yahoo.com>
2020-12-12 21:34:53 -0500

    add comic button added to home page, fav and public/private buttons added to create collection page

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div class="jumbotron jumbotron-fluid shadow">
-      <h1>Comic Collector!</h1>
-      <p>A place to collect and share comics!</p>
+      <h1>Comic League!</h1>
+      <p>Track your collection! Share with your friends!</p>
     </div>
 
     <div class="row shadow">
@@ -57,9 +57,12 @@
       <div class="col-md-2 shadow"> image representing collection
         <p class="row"> collection title </p>
       </div>
-      <div class="col-md-2 shadow"> <router-link class="btn btn-primary" 
-          v-bind:to="{ name: 'newCollection' }" v-show="$store.state.token != ''">Create New Collection</router-link>
-      </div>
+      <span class="contain"> <router-link class="btn btn-primary" 
+        v-bind:to="{ name: 'newCollection' }" v-show="$store.state.token != ''">Create New Collection</router-link>
+        <span> <router-link class="btn btn-success"
+          v-bind:to="{ name: 'newComic' }" v-show="$store.state.token != ''">Add Comic</router-link>
+        </span>
+      </span>
     </div>
 
     <footer>
@@ -75,6 +78,7 @@ export default {
 </script>
 
 <style scoped>
+/* --------------------  header ---------------- */
 .jumbotron {
   font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
   color: white;
@@ -84,6 +88,7 @@ export default {
   padding: 20px;
   border: white solid 6px;
 }
+/* ------------------------ collection sections ------------- */
 div.row {
   background-color:rgba(220, 207, 189, 0.94);
   width: 75%;
@@ -91,12 +96,13 @@ div.row {
   padding: 0px;
   border: white solid 6px;
 }
-div p.row {
-  width: 100%;
-  border-top: darkgrey solid 1px;
-  padding: 5px;
+.col-md-12 {
+  background-color: white;
+  padding: 10px 10px 5px;
   margin: 0px;
+  font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
 }
+/* ------------------ collection boxes ---------- */
 .col-md-2{
   margin: 20px auto;
   padding: 0px;
@@ -104,17 +110,39 @@ div p.row {
   border: darkgrey solid 1px;
   text-align: center;
 }
-.col-md-12 {
-  background-color: white;
-  padding: 10px 10px 5px;
+div p.row {
+  width: 100%;
+  height: 30px;
+  display: block;
+  border-top: darkgrey solid 1px;
+  padding: 5px;
   margin: 0px;
-  font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
 }
+/* ----------- add comic and new collection buttons ------- */
+.contain {
+  width: 15%;
+  margin: 20px auto;
+}
+.btn-primary{
+  width: 100%;
+}
+.btn-success{
+  width: 100%;
+}
+/* ------------- footer ------------ */
 footer {
   text-align:center;
   width: 75%;
   margin:auto;
   padding: 10px;
   background-color: lightgray;
+}
+
+/* --------------- for buttons on a smaller screen -------------- */
+@media only screen and (max-width: 800px) {
+  .contain{
+    width: 75%;
+    margin:auto auto 20px;
+  }  
 }
 </style>
