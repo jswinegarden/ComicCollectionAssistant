ae45e62a46e8c9e11d8b8edcdaaee142c67c25c4
Jonathon Winegarden <jswinegarden@gmail.com>
2020-12-13 20:33:15 -0500

    Implemented my collections in account view

--- vue/src/components/ViewCollection.vue
+++ vue/src/components/ViewCollection.vue
@@ -1,30 +1,14 @@
 <template>
     <div>
-    <div id="Collections">
-        <h1>My Collections</h1>
+    <div id="Collection">
+        <h1>{{ title }}</h1>
             <router-link :to="{ name: 'Account', params: { id: collection.id } }"
             class="collection"
             v-for="collection in this.$store.state.collections"
             v-bind:key="collection.id"
             v-bind:style="{ 'background-color': collection.backgroundColor }"
             >
-            {{ collection.title }}
             </router-link>
-    </div>
-    <div class="row shadow">
-      <h4 class="col-md-12">My Collections:</h4>
-      <div class="col-md-2 shadow"> image representing collection
-        <p class="row"> collection title </p>
-      </div>
-      <div class="col-md-2 shadow"> image representing collection
-        <p class="row"> collection title </p>
-      </div>
-      <div class="col-md-2 shadow"> image representing collection
-        <p class="row"> collection title </p>
-      </div>
-      <div class="col-md-2 shadow"> image representing collection
-        <p class="row"> collection title </p>
-      </div>
     </div>
     </div>
 </template>
@@ -51,29 +35,3 @@ export default {
     }
 }
 </script>
-
-<style scoped>
-.col-md-2{
-  margin: 20px auto;
-  padding: 0px;
-  background-color: white; /*replace with image that represents collection */
-  border: darkgrey solid 1px;
-  text-align: center;
-  font-family: AnimeAce;
-}
-div p.row {
-  width: 100%;
-  height: 30px;
-  display: block;
-  border-top: darkgrey solid 1px;
-  padding: 5px;
-  margin: 0px;
-  font-family: CrashLanding, Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-}
-.col-md-12 .btn{
-  margin: auto;
-  background-color: none;
-  border: none;
-  padding: 5px;
-}
-</style>
--- vue/src/views/Account.vue
+++ vue/src/views/Account.vue
@@ -1,15 +1,85 @@
 <template>
-    
+    <div>
+        <div class="row shadow">
+      <h4 class="col-md-12">My Collections:</h4>
+      <div class="col-md-2 shadow"> image representing collection
+        <p class="row"> collection title </p>
+      </div>
+      <div class="col-md-2 shadow"> image representing collection
+        <p class="row"> collection title </p>
+      </div>
+      <div class="col-md-2 shadow"> image representing collection
+        <p class="row"> collection title </p>
+      </div>
+      <div class="col-md-2 shadow"> image representing collection
+        <p class="row"> collection title </p>
+      </div>
+      <span class="contain"> <router-link class="btn btn-dark" 
+        v-bind:to="{ name: 'newCollection' }" v-show="$store.state.token != ''">Create New Collection</router-link>
+        <span> <router-link class="btn btn-success"
+          v-bind:to="{ name: 'newComic' }" v-show="$store.state.token != ''">Add Comic</router-link>
+        </span>
+      </span>
+    </div>
+    </div>
 </template>
 
 <script>
-import ViewCollection from '../components/ViewCollection.vue'
+import ViewCollection from '../components/ViewCollection';
 export default {
-    name: 'Account',
-    components: { ViewCollection },
-}
+    components: { ViewCollection }
+};
 </script>
 
 <style scoped>
 
+/* ------------------------ collection sections ------------- */
+div.row {
+  background-color:rgba(220, 207, 189, 0.94);
+  width: 75%;
+  margin: 15px auto;
+  padding: 0px;
+  border: white solid 6px;
+}
+.col-md-12 {
+  background-color: white;
+  padding: 10px 10px 5px;
+  margin: 0px;
+  font-family: CrashLanding, Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+}
+/* ------------------ collection boxes ---------- */
+.col-md-2{
+  margin: 20px auto;
+  padding: 0px;
+  background-color: white; /*replace with image that represents collection */
+  border: darkgrey solid 1px;
+  text-align: center;
+  font-family: AnimeAce;
+}
+div p.row {
+  width: 100%;
+  height: 30px;
+  display: block;
+  border-top: darkgrey solid 1px;
+  padding: 5px;
+  margin: 0px;
+  font-family: CrashLanding, Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+}
+.col-md-12 .btn{
+  margin: auto;
+  background-color: none;
+  border: none;
+  padding: 5px;
+}
+/* ----------- add comic and new collection buttons ------- */
+.contain {
+  width: 15%;
+  margin: 20px auto;
+}
+.btn-primary{
+  width: 100%;
+}
+.btn-success{
+  width: 100%;
+}
 </style>
